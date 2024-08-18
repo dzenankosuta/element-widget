d5a259f74053248dfc92cbff00d0c220a0f05280
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-08-18 12:26:16 +0200

    show components in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./App.css";
+import SelectedItems from "./components/SelectedItems";
+import SelectItemsDialog from "./components/SelectItemsDialog";
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Element Widget</h1>
+      <h1 className="app-title">Element Widget</h1>
+      <SelectedItems />
+      <SelectItemsDialog />
     </div>
   );
 };
